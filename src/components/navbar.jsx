eb9e1b65986cbd29f1e591476e49968cb627c4e6
AngelDST13 <angeldanielasalazart13@gmail.com>
2026-09-03 15:15:49 -0600

    feat: agregar logo de El Patron al navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,44 +1,33 @@
 import { Link } from 'react-router-dom'
+import logoImg from '../img/logo.png'
 
 function Navbar() {
   return (
     <header className="navbar">
-
       <div className="navbar-container">
 
-        <Link
-          to="/"
-          className="navbar-logo"
-        >
-          <span>EL PATRÓN</span>
-          <small>LUXURY BARBERSHOP</small>
+        <Link to="/" className="navbar-logo">
+          <img 
+            src={logoImg} 
+            alt="El Patrón Logo" 
+            style={{ height: '40px', width: 'auto', objectFit: 'contain', marginRight: '10px' }} 
+          />
+          <div>
+            <span>EL PATRÓN</span>
+            <small>LUXURY BARBERSHOP</small>
+          </div>
         </Link>
 
         <nav className="navbar-menu">
-
-          <a href="#servicios">
-            Servicios
-          </a>
-
-          <a href="#promociones">
-            Promociones
-          </a>
-
-          <a href="#reservar">
-            Reservar
-          </a>
-
-          <Link
-            to="/login"
-            className="navbar-login"
-          >
+          <a href="#servicios">Servicios</a>
+          <a href="#promociones">Promociones</a>
+          <a href="#reservar">Reservar</a>
+          <Link to="/login" className="navbar-login">
             Personal
           </Link>
-
         </nav>
 
       </div>
-
     </header>
   )
 }
